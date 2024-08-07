7f84fbd56a76f79991b7fda57beac5be7efa83bd
hxxchxx <a01068166050@gmail.com>
2024-08-07 14:16:13 +0900

    birthCulcularot

---
+++ birthCulculator.js
@@ -0,0 +1,8 @@
+var currentYear = 2024;
+var birthYeaer;
+var age;
+
+birthYeaer = prompt ("태어난 연도를 입력하세요. (YYYY)","");
+age = currentYeaer - birthYeaer + 1;
+document.write(currentYear + "년 현재<br>");
+document.write(birthYeaer + "년에 태어난 사람의 나이는 " + age + "세입니다.");
